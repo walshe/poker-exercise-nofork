8408b4032af2be175ee058c46e7c6a06ae84b99b
Dónal Murtagh <donal.murtagh@clarusonesourcing.com>
2024-04-19 19:48:35 +0100

    remove reference to deleted class in comment

--- src/main/java/com/clarusone/poker/PokerHand.java
+++ src/main/java/com/clarusone/poker/PokerHand.java
@@ -3,13 +3,12 @@ package com.clarusone.poker;
 public class PokerHand implements Comparable<PokerHand> {
 
     public PokerHand(String fiveCards) {
-
     }
 
     @Override
     public int compareTo(PokerHand opponentHand) {
         /*
-        TODO Delete the line below and implement this method to return HandResult.WIN, HandResult.LOSS, or HandResult.TIE,
+        TODO Delete the line below and implement this method to return a positive integer, a negative integer, or zero 
         if this hand is better than, worse than, or the same as the opponent's hand
          */
         throw new UnsupportedOperationException("Hand comparison not implemented");
