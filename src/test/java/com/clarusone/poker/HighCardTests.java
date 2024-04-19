4b133d142a3a9a2221b462d167e78d94008a7c52
Dónal Murtagh <donal.murtagh@clarusonesourcing.com>
2024-04-19 23:26:32 +0100

    remove public

--- src/test/java/com/clarusone/poker/HighCardTests.java
+++ src/test/java/com/clarusone/poker/HighCardTests.java
@@ -11,17 +11,17 @@ import static com.clarusone.poker.HandComparisonUtils.assertWin;
 public class HighCardTests {
 
     @Test
-    public void first_highest_card_wins() {
+    void first_highest_card_wins() {
         assertWin("7H 3C TH 6H 9S", "2S 3H 6H 7S 9C");
     }
 
     @Test
-    public void fifth_highest_card_wins() {
+    void fifth_highest_card_wins() {
         assertWin("4S 5H 6H TS AC", "3S 5H 6H TS AC");
     }
 
     @Test
-    public void equal_cards_tie() {
+    void equal_cards_tie() {
         assertTie("2S AH 4H 5S 6C", "AD 4C 5H 6H 2C");
     }
 }
